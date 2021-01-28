e9fb4969445e43a2d230ba984b2c6a6eeba8e327
Rasooll <safari@rasooll.com>
2021-01-28 14:55:07 +0330

    Fix.

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-# Change desktop wallpaper to bing.com
+# Change your wallpaper to bing.com
 
-<strong>dependency:</strong>
-<code>Wget, Curl</code><br />
+## Requirements:
+- gnome-shell
+- wget
 
-<strong>before run:</strong><br>
-<code>$ mkdir $HOME/Pictures/Wallpapers/bing/ir</code><br>
-<code>$ mkdir $HOME/Pictures/Wallpapers/bing/us</code><br>
-<code>$ mkdir $HOME/Pictures/Wallpapers/bing/gb</code><br>
-
-<strong>how to run:</strong><br>
-<code>$ ./bing.sh ir</code><br>
-<code>$ ./bing.sh us</code><br>
-<code>$ ./bing.sh gb</code><br>
+# How to use:
+Just run it.
+```sh
+bash bing.sh
+```
--- bing.sh
+++ bing.sh
@@ -1,78 +1,21 @@
 #!/bin/bash
-case $1 in
-	"us")
-		dates=`date +%Y-%m-%d`
-		function getpic() {
-			curl "https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-US" > /tmp/bing.xml
-			cat /tmp/bing.xml | grep -oP "<url>\/[a-zA-z]+.+<\/url>" | grep -oP "\/az+.[a-zA-z]+.+.jpg" > /tmp/esm.txt
-			links="https://bing.com"`cat /tmp/esm.txt`
-			wget $links -O $HOME/Pictures/Wallpapers/bing/us/$dates.jpg
-			gsettings set org.gnome.desktop.background picture-uri file://$HOME/Pictures/Wallpapers/bing/us/$dates.jpg
-		}
-		if [[ -f $HOME/Pictures/Wallpapers/bing/us/$dates.jpg ]]; then
-			echo "This image currently exist."
-			echo "Do you want remove this picture? (y,n) "
-			echo "**if enter (n) change background to this picture"
-			read removethis
-			if [[ $removethis == "y" ]]; then
-				rm $HOME/Pictures/Wallpapers/bing/us/$dates.jpg
-				getpic
-			elif [[ $removethis == "n" ]]; then
-				gsettings set org.gnome.desktop.background picture-uri file://$HOME/Pictures/Wallpapers/bing/us/$dates.jpg
-			fi
-		else
-			getpic
-		fi
-	;;
-	"ir")
-		dates=`date +%Y-%m-%d`
-		function getpic() {
-			curl "https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=fa-IR" > /tmp/bing.xml
-			cat /tmp/bing.xml | grep -oP "<url>\/[a-zA-z]+.+<\/url>" | grep -oP "\/az+.[a-zA-z]+.+.jpg" > /tmp/esm.txt
-			links="https://bing.com"`cat /tmp/esm.txt`
-			wget $links -O $HOME/Pictures/Wallpapers/bing/ir/$dates.jpg
-			gsettings set org.gnome.desktop.background picture-uri file://$HOME/Pictures/Wallpapers/bing/ir/$dates.jpg
-		}
-		if [[ -f $HOME/Pictures/Wallpapers/bing/ir/$dates.jpg ]]; then
-			echo "This image currently exist."
-			echo "Do you want remove this picture? (y,n) "
-			echo "**if enter (n) change background to this picture"
-			read removethis
-			if [[ $removethis == "y" ]]; then
-				rm $HOME/Pictures/Wallpapers/bing/ir/$dates.jpg
-				getpic
-			elif [[ $removethis == "n" ]]; then
-				gsettings set org.gnome.desktop.background picture-uri file://$HOME/Pictures/Wallpapers/bing/ir/$dates.jpg
-			fi
-		else
-			getpic
-		fi
-	;;
-	"gb")
-		dates=`date +%Y-%m-%d`
-		function getpic() {
-			curl "https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-GB" > /tmp/bing.xml
-			cat /tmp/bing.xml | grep -oP "<url>\/[a-zA-z]+.+<\/url>" | grep -oP "\/az+.[a-zA-z]+.+.jpg" > /tmp/esm.txt
-			links="https://bing.com"`cat /tmp/esm.txt`
-			wget $links -O $HOME/Pictures/Wallpapers/bing/gb/$dates.jpg
-			gsettings set org.gnome.desktop.background picture-uri file://$HOME/Pictures/Wallpapers/bing/gb/$dates.jpg
-		}
-		if [[ -f $HOME/Pictures/Wallpapers/bing/gb/$dates.jpg ]]; then
-			echo "This image currently exist."
-			echo "Do you want remove this picture? (y,n) "
-			echo "**if enter (n) change background to this picture"
-			read removethis
-			if [[ $removethis == "y" ]]; then
-				rm $HOME/Pictures/Wallpapers/bing/gb/$dates.jpg
-				getpic
-			elif [[ $removethis == "n" ]]; then
-				gsettings set org.gnome.desktop.background picture-uri file://$HOME/Pictures/Wallpapers/bing/gb/$dates.jpg
-			fi
-		else
-			getpic
-		fi
-	;;
-	*)
-	echo "Error! Invalid Location."
-	;;
-esac
+
+dates=`date +%Y-%m-%d`
+function getpic() {
+	mkdir -p $HOME/Pictures/Wallpapers/bing
+	picture_path=$(wget -qO- "https://www.bing.com/HPImageArchive.aspx?n=1" | grep -oP "<url>\/[a-zA-z]+.+<\/url>" | sed 's/<url>//g' | sed 's/<\/url>//g')
+	wget -q "https://bing.com$picture_path" -O $HOME/Pictures/Wallpapers/bing/$dates.jpg
+	gsettings set org.gnome.desktop.background picture-uri file://$HOME/Pictures/Wallpapers/bing/$dates.jpg
+	echo Done
+}
+if [[ -f $HOME/Pictures/Wallpapers/bing/$dates.jpg ]]; then
+	echo "This image currently exist."
+	echo "Do you want remove this picture? (y,n) "
+	read removethis
+	if [[ $removethis == "y" ]]; then
+		rm $HOME/Pictures/Wallpapers/bing/$dates.jpg
+		getpic
+	fi
+else
+	getpic
+fi
